db7ca59d0403671fd5f73430b857df7c32f55517
Mike Bamford <mb2174@cam.ac.uk>
2020-10-10 07:38:02 +0100

    override billing account with local

--- project/main.tf
+++ project/main.tf
@@ -2,6 +2,7 @@ module "sunnyportal_download_project" {
   source = "git::https://github.com/msb/tf-gcp-project.git"
 
   project_name         = "Sunny Portal"
+  billing_account_name = local.billing_account_name
   additional_apis      = [
     "drive.googleapis.com",
   ]
